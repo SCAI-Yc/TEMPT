4edca435d5d7143f0763f33ef56eed3080c506a1
SCAI-Yc <12232418@mail.sustech.edu.cn>
2025-07-28 08:54:19 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -5,13 +5,7 @@ import glob
 import re
 import torch
 from torch.utils.data import Dataset, DataLoader
-# from sklearn.preprocessing import StandardScaler
-# from utils.timefeatures import time_features
-# from data_provider.m4 import M4Dataset, M4Meta
-# from data_provider.uea import subsample, interpolate_missing, Normalizer
-# from sktime.datasets import load_from_tsfile_to_dataframe
 import warnings
-# from mambaConfig import mambaConfig
 
 class TrajDataset(Dataset):
     def __init__(self, dataset='CD', data_path_prefix="./data/",
@@ -26,16 +20,12 @@ class TrajDataset(Dataset):
 
         self.seq_len = size[0]
         self.pred_len = size[1]
-        # self.train_split = split[0] / sum(split)
-        # self.test_split = split[1] / sum(split)
-        # self.val_split = split[2] / sum(split)
         self.__init_data__()
 
     def __init_data__(self):
         # if self.flag == "train":
         dataset = np.load(self.data_path_prefix + self.data_path)
 
-        # full_data = np.load(self.data_path)
         self.pred_pairs = dataset[:, :, :2]
 
     def __getitem__(self, index):
@@ -48,27 +38,6 @@ class TrajDataset(Dataset):
         return len(self.pred_pairs)
     
     
-
-class MambaDataset(Dataset):
-    def __init__(self, data_path, size=[400, 10], flag='train', dataset='XA', seed=2025):
-        super().__init__()
-        self.data_path = "/data1/jyc/mixer/{}_{}_".format(dataset, flag) + data_path
-        self.seq_len = size[0]
-        self.pred_len = size[1]
-        self.flag = flag
-
-        self.__init_data__()
-
-    def __init_data__(self):
-        # if self.flag == "train":
-        dataset = np.load(self.data_path)
-
-        # full_data = np.load(self.data_path)
-        self.pred_pairs = dataset[:, :, :2]
-
-    def __getitem__(self, index):
-        input_seq = self.pred_pairs[index, :self.seq_len, :2]
-        pred_seq = self.pred_pairs[index, -self.pred_len:, :2]
 
         return input_seq, pred_seq
     
